f78b8c81a983c407b899870f7edf5339e23c9eca
dan <danielmartins8@hotmail.com>
2019-07-09 20:28:27 +0100

    gregory update and report

--- Concorrente2/src/GregoryAkka.java
+++ Concorrente2/src/GregoryAkka.java
@@ -18,25 +18,25 @@ public class GregoryAkka {
     @SuppressWarnings("serial")
 	public static class CalculaIntervaloMessage implements Serializable {
         public final long num;
-        public final ActorRef actorprimo;
-        public CalculaIntervaloMessage(long num, ActorRef actorprimo) {
+        public final ActorRef actorPI;
+        public CalculaIntervaloMessage(long num, ActorRef actorPI) {
             this.num = num;
-            this.actorprimo = actorprimo;
+            this.actorPI = actorPI;
         }
     }
     
     @SuppressWarnings("serial")
-	public static class CalculaPrimoMessage implements Serializable {
+	public static class CalculaPIcalcMessage implements Serializable {
         public final long num;
-        public CalculaPrimoMessage(long num) {
+        public CalculaPIcalcMessage(long num) {
             this.num = num;
         }
     }
     @SuppressWarnings("serial")
-	public static class RespostaPrimoMessage implements Serializable {
-        public final double eprimo;
-        public RespostaPrimoMessage(double i) {
-            this.eprimo = i;
+	public static class RespostaPIcalcMessage implements Serializable {
+        public final double calcpi;
+        public RespostaPIcalcMessage(double i) {
+            this.calcpi = i;
         }
     }
     @SuppressWarnings("serial")
@@ -50,7 +50,7 @@ public class GregoryAkka {
     public static class Controlador extends UntypedActor {
         long contaRespostas;
         double sum = 0;
-        ActorRef actorPrimo;
+        ActorRef actorPI;
         ActorRef actorIniciador;
 
         public void onReceive(Object message) {
@@ -58,86 +58,78 @@ public class GregoryAkka {
           if (message instanceof CalculaIntervaloMessage) {	
         	  
             this.contaRespostas= ((CalculaIntervaloMessage) message).num;
-            this.actorPrimo = ((CalculaIntervaloMessage) message).actorprimo;
+            this.actorPI = ((CalculaIntervaloMessage) message).actorPI;
             this.actorIniciador = getSender();
             	
             for (long i=1; i<=contaRespostas; i++){
-              actorPrimo.tell( new CalculaPrimoMessage(i), getSelf() );
+              actorPI.tell( new CalculaPIcalcMessage(i), getSelf() );
               if(i==contaRespostas)System.out.println("FIM");
             }
             	
-            	System.out.println("Controlador: envio CalculaPrimoMessage " + contaRespostas);
+            	System.out.println("Controlador: envio CalculaPIcalcMessage " + contaRespostas);
 
-            } else if (message instanceof RespostaPrimoMessage) {
+            } else if (message instanceof RespostaPIcalcMessage) {
             	
-            	if ( ((RespostaPrimoMessage) message).eprimo >0 )
-            		sum = ((RespostaPrimoMessage) message).eprimo;
-            	
-            	contaRespostas -= 1;
-            	double pi;
-    			pi =4.0*sum;
-            	
-    			System.out.println("SUM "+sum);
-            //	System.out.println("Controlador: ResultadoMessage "+pi);
-    			if (contaRespostas == 0)
-            		actorIniciador.tell( 
-            			new QuantidadeIntervaloMessage(pi), getSelf() );
+            	if ( ((RespostaPIcalcMessage) message).calcpi != 0 ){
+                    sum = sum+((RespostaPIcalcMessage) message).calcpi;
+                    contaRespostas -= 1;
+                }
+
+    			//System.out.println("sum "+ sum);
+                System.out.println("sum " + contaRespostas);
+
+    			if (contaRespostas == 0){
+                    double pi;
+                    pi = 4.0 * (1+sum);
+                    System.out.println("PI " + pi);
+                    actorIniciador.tell(new QuantidadeIntervaloMessage(pi), getSelf());
+                }
+
             	
             } else unhandled(message);
     			
         }
+
     }
 
-    public static class Primo extends UntypedActor {
+    public static class PIcalc extends UntypedActor {
     	
         public void onReceive(Object message) {
         	
-        	if (message instanceof CalculaPrimoMessage) {
+        	if (message instanceof CalculaPIcalcMessage) {
         		
-        		long num = ((CalculaPrimoMessage) message).num;
-        		
+        		long num = ((CalculaPIcalcMessage) message).num;
 
-        				
-        		System.out.println("Primo: CalculaPrimoMessage " + num );
-        		
         		double f = Calcula(num);
-        		getSender().tell(new RespostaPrimoMessage(f),getSelf());
-        		System.out.println(Calcula(num));
+        		getSender().tell(new RespostaPIcalcMessage(f),getSelf());
+        		//System.out.println(f);
         	} else unhandled(message);
         }
     }
    
-    public static boolean isPrime(long number)
-	{ 
-	  if (number < 2) return false; 
-	  for (long i=2; i<number; i++) 
-	    if (number % i == 0) return false; 
-	  return true; 
-	} 
+
     public static boolean isPar(long number)
    	{ 
-   		if(number%2==0){
-   			return true;
-   		}else{
-   			return false;
-   			
-   		}
+            if(number%2==0){
+                return true;
+            }else{
+                return false;
+                
+            }
    		}
     public static double Calcula(long number){
-    	long k;
+    
     	double factor;
-    	double sum=0;
-    	for(k=0;k<number;k++){
-			if(!isPar(k)){
+    	double value=0;
+    	
+			if(!isPar(number)){
 				factor=-1.0;
 			}else{
 				factor=1.0;
 			}
-			sum+=factor/(2*k+1);
-			//factor=-factor;	
-			
-    	}
-    	return(sum);
+			value+=factor/(2* number+1);
+
+    	return(value);
     }
 
     
@@ -153,12 +145,9 @@ public class GregoryAkka {
         // Create the 'controlador' actor
         final ActorRef controlador = sistema.actorOf(Props.create(Controlador.class), "controlador");
 
-        // Create the 'Primo' actor
-//        final ActorRef primo = sistema.actorOf(Props.create(Primo.class), "primo");
-
         // Create the Route actor; getContext() from actor
         // http://doc.akka.io/docs/akka/2.3.16/java/routing.html
-        final ActorRef primo = sistema.actorOf(new RoundRobinPool(4).props(Props.create(Primo.class)), 
+        final ActorRef PIcalc = sistema.actorOf(new RoundRobinPool(4).props(Props.create(PIcalc.class)), 
     		    "router");
     
         // definir tamanho conjunto
@@ -166,7 +155,7 @@ public class GregoryAkka {
         
         // Ask the 'greeter for the latest 'greeting'
         // Reply should go to the "actor-in-a-box"
-        inbox.send(controlador, new CalculaIntervaloMessage( N, primo));
+        inbox.send(controlador, new CalculaIntervaloMessage( N, PIcalc));
         
         // Wait 5 seconds for the reply with the 'greeting' message
         QuantidadeIntervaloMessage resposta = (QuantidadeIntervaloMessage) 
